1c05f82df687886ee88fcac6a22d7a9da7f33978
BrayanT <brayantorresacuna2023@gmail.com>
2023-09-05 20:30:11 -0500

    Crear clase Libro

---
+++ src/main/java/com/pe/biblioteca/modelo/Libro.java
@@ -0,0 +1,31 @@
+package com.pe.biblioteca.modelo;
+
+public class Libro {
+    private String nombre;
+    private double precio;
+    
+    public Libro(){
+        
+    }
+
+    public Libro(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
