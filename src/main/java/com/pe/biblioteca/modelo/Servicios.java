512dd47a36a166ded75a5e0c980f32bb5eb31f21
LuisPerez2008 <113653537+LuisPerez2008@users.noreply.github.com>
2023-09-05 22:59:07 -0500

    prototipo del menu ojala q salga bien el pull xdxdxd

---
+++ src/main/java/com/pe/biblioteca/modelo/Servicios.java
@@ -0,0 +1,21 @@
+package com.pe.biblioteca.modelo;
+
+public class Servicios {
+
+
+    //ordenamiento burbuja por precio
+    public static void ordenarPorPrecio(Libro[] lb) {
+        int n = lb.length;
+        for (int i = 0; i < lb.length - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (lb[j].getPrecio() > lb[j + 1].getPrecio()) {
+                    Libro temp = lb[j];
+                    lb[j] = lb[j + 1];
+                    lb[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+}
